9a439a7d6a1deabf415e969fc519142c0d7d0655
CarlosVilla25 <ing.cavij@gmail.com>
2021-12-11 00:41:21 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import PokeCard from './components/PokeCard'
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [pokemon, setPokemon] = useState([])
+
+  const fetchData = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+      .then(res => res.json())
+      .then(({ results }) => {
+        results.forEach((pokemon) => {
+          fetch(pokemon.url)
+            .then(res => res.json())
+            .then(data => {
+              setPokemon(prev => [...prev, data])
+            })
+        })
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div className="container-title">
+        <h1><a href="https://pokeapi.co/">POKE API</a></h1>
+      </div>
+      <PokeCard data={pokemon} />
+      <div className='app-footer'>Made with ❤ by <a href="https://github.com/CarlosVilla25">Charlie Villa</a></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
